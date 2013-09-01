20d822d66b3b2ef0fac060c373e7407ffeb9c667
hmsintrepide <quentin.gallissot@orange.fr>
2013-09-01 14:35:06 +0200

    update library & add theme and language save
    update jquery to 1.10.2,
    update base64 library,
    update rawdeflate/inflate library,
    update ace editor,
    replace hash (index.html#mystring) save with post like var
    (index.html?myvar=content&mysecondvar=content-2).

--- js/shortly.js
+++ js/shortly.js
@@ -1,7 +1,7 @@
 (function()	{
 	$(function() {
-		var editor = ace.edit("input-field");
-		editor.setTheme("ace/theme/chrome");
+		var editor = ace.edit('input-field');
+		editor.setTheme('ace/theme/chrome');
 		editor.getSession().setMode('ace/mode/text');
 		var base64, check, checksum, deflated, encoded, input;
 		checksum = function(string) {
@@ -14,31 +14,53 @@
 			return chk % 10;
 		};
 		$('#save-button').click(function() {
-			var base64, check, deflated, encoded, input;
 			input = editor.getValue();
 			deflated = RawDeflate.deflate(input);
 			base64 = Base64.toBase64(deflated);
 			check = checksum(base64);
 			encoded = base64 + check;
-			window.location = 'index.html#' + encoded;
-			document.getElementById("view").style.display = "block";
-			return $('#input-url').get(0).value = window.location;
+			window.location = 'index.html?d=' + encoded + '&l=' + $('#language').val() + '&t=' + $('#theme').val();
+			document.getElementById('view').style.display = 'block';
+			$('#input-url').get(0).value = window.location;
 		});
-		if (window.location.hash) {
-			document.getElementById("view").style.display = "block";
-			encoded = window.location.hash.replace(/^#/, '');
+		$('#theme').change(function() {
+			editor.setTheme('ace/theme/' + $('#theme').val());
+		});
+		$('#language').change(function() {
+			editor.getSession().setMode('ace/mode/' + $('#language').val());
+		});
+		if (getUrlVars()['d']) {
+			document.getElementById('view').style.display = 'block';
+			encoded = getUrlVars()['d'];
 			base64 = encoded.slice(0, (encoded.length - 2) + 1 || 9e9);
 			check = parseInt(encoded.slice(encoded.length - 1, encoded.length + 1 || 9e9));
 			if (check !== checksum(base64)) {
 				$('#input-url').get(0).value = 'Something got corrupted :(';
-				return editor.setValue('Something got corrupted :(');
+				editor.setValue('Something got corrupted :(');
 			} else {
 				deflated = Base64.fromBase64(base64);
 				input = RawDeflate.inflate(deflated);
 				$('#input-url').get(0).value = window.location;
 				editor.setValue(input);
-				return editor.clearSelection();
+				editor.clearSelection();
 			}
+		}
+		if (getUrlVars()['t']) {
+			editor.setTheme('ace/theme/' + getUrlVars()['t']);
+			document.getElementById('theme').selectedIndex = 0;
+			document.getElementById('theme').value = getUrlVars()['t'];
+		}
+		if (getUrlVars()['l']) {
+			editor.getSession().setMode('ace/mode/' + getUrlVars()['l']);
+			document.getElementById('language').selectedIndex = 0;
+			document.getElementById('language').value = getUrlVars()['l'];
 		}
 	});
 }).call(this);
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+		vars[key] = value;
+	});
+	return vars;
+}
