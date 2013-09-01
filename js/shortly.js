88a28ac1a0f6bbabd5cf9b7eab2cd8b7e2589d7f
hmsintrepide <quentin.gallissot@orange.fr>
2013-09-01 21:49:41 +0200

    add more themes and languages

--- js/shortly.js
+++ js/shortly.js
@@ -1,8 +1,8 @@
 (function()	{
 	$(function() {
 		var editor = ace.edit('input-field');
-		editor.setTheme('ace/theme/chrome');
-		editor.getSession().setMode('ace/mode/text');
+		editor.setTheme('ace/theme/' + document.getElementById('theme').value);
+		editor.getSession().setMode('ace/mode/' + document.getElementById('language').value);
 		var base64, check, checksum, deflated, encoded, input;
 		checksum = function(string) {
 			var chk, chr, i;
@@ -13,14 +13,21 @@
 			}
 			return chk % 10;
 		};
+		getUrlVars = function() {
+			var vars = {};
+			var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+				vars[key] = value;
+			});
+			return vars;
+		}
 		$('#save-button').click(function() {
 			input = editor.getValue();
 			deflated = RawDeflate.deflate(input);
 			base64 = Base64.toBase64(deflated);
 			check = checksum(base64);
 			encoded = base64 + check;
-			window.location = 'index.html?d=' + encoded + '&l=' + $('#language').val() + '&t=' + $('#theme').val();
-			document.getElementById('view').style.display = 'block';
+			window.location = 'index.html?l=' + $('#language').val() + '&t=' + $('#theme').val() + '&d=' + encoded;
+			$('#view').show();
 			$('#input-url').get(0).value = window.location;
 		});
 		$('#theme').change(function() {
@@ -30,7 +37,7 @@
 			editor.getSession().setMode('ace/mode/' + $('#language').val());
 		});
 		if (getUrlVars()['d']) {
-			document.getElementById('view').style.display = 'block';
+			$('#view').show();
 			encoded = getUrlVars()['d'];
 			base64 = encoded.slice(0, (encoded.length - 2) + 1 || 9e9);
 			check = parseInt(encoded.slice(encoded.length - 1, encoded.length + 1 || 9e9));
@@ -57,10 +64,3 @@
 		}
 	});
 }).call(this);
-function getUrlVars() {
-	var vars = {};
-	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-		vars[key] = value;
-	});
-	return vars;
-}
